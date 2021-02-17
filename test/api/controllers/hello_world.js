b90b4a697494eb439d7e9edbaaf73f27b6af4086
YURIKA TAKADA <EUREKAT@jp.ibm.com>
2021-02-17 08:35:24 +0000

    updating the code to use the "Hello there" message

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, stranger!');
+            res.body.should.eql('Hello there, stranger!');
 
             done();
           });
